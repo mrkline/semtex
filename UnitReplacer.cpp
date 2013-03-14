b8961e2fa9f5409a868a57f04eb6865f73350c18
Matt Kline <slavik262@gmail.com>
2013-03-14 08:49:11 -0500

    \unit argument is now just named "u"
    Also improved error message for \unit

--- UnitReplacer.cpp
+++ UnitReplacer.cpp
@@ -22,10 +22,10 @@ void UnitReplacer::replace(const std::string& matchedKey, ParseInfo& pi)
 	std::string* unit;
 	if (args->unnamed.size() == 1 && args->named.size() == 0)
 		unit = &args->unnamed[0];
-	else if (args->unnamed.size() == 0 && args->named.size() == 1 && args->named.find("unit") != args->named.end())
-		unit = &args->named["unit"];
+	else if (args->unnamed.size() == 0 && args->named.size() == 1 && args->named.find("u") != args->named.end())
+		unit = &args->named["u"];
 	else
-		errorOnLine(pi, "Incorrect argument(s) for \\unit");
+		errorOnLine(pi, "Incorrect argument(s) for \\unit.\n\t\\unit takes a single argument, optionally named \"u\".");
 
 	const char* end = pi.curr;
 	pi.replacements.emplace_back(start, end, "\\,\\mathrm{" + *unit + "}");
